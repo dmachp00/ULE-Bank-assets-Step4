e8e1b7d7e765a3b295f187ea65ffdb5f66d1e1de
dmachp00 <dmachp00@estudiantes.unileon.es>
2014-06-03 23:50:50 +0200

    Setp 5 y 6 inacabado

--- src/main/java/com/companyname/springapp/service/SimpleLoanIteratorManager.java
+++ src/main/java/com/companyname/springapp/service/SimpleLoanIteratorManager.java
@@ -2,30 +2,43 @@ package com.companyname.springapp.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import es.unileon.ulebank.assets.iterator.LoanIterator;
+import com.companyname.springapp.repository.LoanIteratorDao;
 
 public class SimpleLoanIteratorManager implements LoanIteratorManager {
 	
 	private static final long serialVersionUID = 1L;
 	
-	private List<LoanIterator> loanIterators;
+	 @Autowired
+	    private LoanIteratorDao loanIteratorDao;
+
+	    public void setProductDao(LoanIteratorDao loanIteratorDao) {
+	        this.loanIteratorDao = loanIteratorDao;
+	    }
+	
+	private LoanIteratorManager loanIterators;
 	
 	@Override
 	public List<LoanIterator> getLoanIterators() {
-		return loanIterators;
+		return loanIteratorDao.getLoanIteratorDoaList();
 	}
 
 	@Override
 	public void recalculateInterest(int percentage) {
+		List<LoanIterator> loanIterators = loanIteratorDao.getLoanIteratorDoaList();
 		if (loanIterators != null) {
 			for (LoanIterator loanIterator : loanIterators) {
 				double newTotals = loanIterator.getTotals().doubleValue() * (100 + percentage)/100;
 				loanIterator.setTotals(newTotals);
+				 loanIteratorDao.saveLoanIteratorDoa(loanIterators);
 			}
 		}
 	}
 
-	public void setLoanIterators(List<LoanIterator> loanIterators) {
+	public void setLoanIterators(LoanIteratorManager loanIterators) {
 		this.loanIterators = loanIterators;
 	}
 
--- src/main/java/es/unileon/ulebank/assets/iterator/LoanIterator.java
+++ src/main/java/es/unileon/ulebank/assets/iterator/LoanIterator.java
@@ -2,14 +2,41 @@ package es.unileon.ulebank.assets.iterator;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import java.util.Iterator;
 import java.util.List;
 
 import es.unileon.ulebank.assets.strategy.loan.ScheduledPayment;
 
+@Entity
+@Table(name="loanIteratorDoas") 
 public class LoanIterator implements Serializable, Iterator<ScheduledPayment> {
 	
 	private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    
+	private String tableOfAmortization;
+	private Double totals;
+
+    public Integer getId()
+    {
+        return id;
+    }
+
+    public void setId(Integer id)
+    {
+        this.id = id;
+    } 
 
 	/**
 	 * List where the fees of a loan are stored
@@ -20,8 +47,7 @@ public class LoanIterator implements Serializable, Iterator<ScheduledPayment> {
 	 */
 	private int position;
 	
-	private String tableOfAmortization;
-	private Double totals;
+
 	
 	public String getTableOfAmortization() {
 		return tableOfAmortization;
