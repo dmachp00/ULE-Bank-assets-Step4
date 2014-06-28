10f362eebbc5ed175721de6b96c32def98ad7b61
dmachp00 <dmachp00@estudiantes.unileon.es>
2014-06-28 20:16:53 +0200

    Nueva Date Base

--- db/load_data.sql
+++ db/load_data.sql
@@ -1,1 +1,7 @@
-INSERT INTO loan (id, money, interest, numFees) values(1, 3000.00, 0.05, 12);
+INSERT INTO bank (bankId) VALUES ('1234');
+INSERT INTO office (officeId, bankId) VALUES ('1234','1234');
+INSERT INTO client (idClient, name, surname) VALUES ('71716254T', 'Dani', 'Machado');
+INSERT INTO account (accountNumber) VALUES ('123412340000000000');
+
+
+INSERT INTO loans (idLoan, idClient, accountNumber, paymentPeriod, initialCapital, amortizationTime, interest, creationDate, description) values('MG-4-2014-ES-KMW2C-7','71716254T', '123412340000000000', 'Monthly', 10000 , 12, 0.05, now(), 'Prestamo atico' );
--- db/springapp_v1.sql
+++ db/springapp_v1.sql
@@ -1,17 +1,50 @@
-CREATE DATABASE springapp;
+CREATE DATABASE ulebank;
 
-GRANT ALL ON springapp_v1.* TO springappuser@'%' IDENTIFIED BY 'pspringappuser';
-GRANT ALL ON springapp_v1.* TO springappuser@localhost IDENTIFIED BY 'pspringappuser';
+GRANT ALL ON ulebank.* TO springappuser@'%' IDENTIFIED BY 'pspringappuser';
+GRANT ALL ON ulebank.* TO springappuser@localhost IDENTIFIED BY 'pspringappuser';
 
-USE springapp;
-
-CREATE TABLE loan (
-  idLoan varchar(255) PRIMARY KEY,
-  idCliente varchar(255),
-  paymentPeriod DOUBLE,
-  initialCapital DOUBLE,
-  amortizationTime DOUBLE,
-  Iterest DOUBLE,
-  tableOfAmortization varchar(500)
+USE ulebank;
+CREATE TABLE loans(
+	idLoan varchar(30) PRIMARY KEY,
+	idClient varchar(9) REFERENCES client(idClient),
+	accountNumber varchar(18) REFERENCES account(accountNumber),
+	paymentPeriod varchar(15),
+	initialCapital DOUBLE,
+	amortizationTime DOUBLE,
+	interest DOUBLE,
+	creationDate datetime,
+	description varchar(160)
 );
-CREATE INDEX loan_tableOfAmortization ON loan(tableOfAmortization);
+
+CREATE TABLE payments(
+	paymentId varchar(15) PRIMARY KEY,
+	idLoan varchar(30) REFERENCES loans(idLoan),
+	dateForPay date,
+	dateLimit date,
+	amountOfMoney DOUBLE,
+	amortization DOUBLE,
+	interest DOUBLE,
+	pendingCapital DOUBLE,
+	isPaid boolean
+);
+
+CREATE TABLE client(
+	idClient varchar(9) PRIMARY KEY,
+	name varchar(15),
+	surname varchar(30)
+);
+
+CREATE TABLE account(
+	accountNumber varchar(18) PRIMARY KEY 
+);
+
+CREATE TABLE bank(
+	bankId varchar(4) PRIMARY KEY
+);
+CREATE TABLE office(
+	officeId varchar(4) PRIMARY KEY,
+	bankId varchar(4) REFERENCES bank(bankIdentificator)
+);
+
+
+CREATE INDEX loans_description ON loans(description);
